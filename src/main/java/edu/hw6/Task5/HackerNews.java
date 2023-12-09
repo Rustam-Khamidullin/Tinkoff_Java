3fd9e1089cbe5ee06f3e355b437dbee24504c2a4
Rustam-Khamidullin <rstm.khamidullin@gmail.com>
2023-12-10 00:46:08 +0700

    hw6: little fix

--- src/main/java/edu/hw6/Task5/HackerNews.java
+++ src/main/java/edu/hw6/Task5/HackerNews.java
@@ -5,7 +5,6 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
