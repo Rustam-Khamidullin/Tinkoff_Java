8bc5cb4e6b71c3a37ca056fa72e93c4be8415634
Rustam-Khamidullin <rstm.khamidullin@gmail.com>
2023-12-18 02:27:27 +0700

    project5: ready for PR

---
+++ src/main/java/edu/project5/ComparePerformance.java
@@ -0,0 +1,98 @@
+package edu.project5;
+
+import java.lang.invoke.LambdaMetafactory;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.concurrent.TimeUnit;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
+
+@State(Scope.Thread)
+public class ComparePerformance {
+    private Student student;
+    private Method method;
+    private MethodHandle methodHandle;
+    private StudentInterface lambda;
+
+    public static void main(String[] args) throws RunnerException {
+        Options options = new OptionsBuilder()
+            .include(ComparePerformance.class.getSimpleName())
+            .shouldFailOnError(true)
+            .shouldDoGC(true)
+            .mode(Mode.AverageTime)
+            .timeUnit(TimeUnit.NANOSECONDS)
+            .forks(1)
+            .warmupForks(1)
+            .warmupIterations(1)
+            .warmupTime(TimeValue.seconds(5))
+            .measurementIterations(2)
+            .measurementTime(TimeValue.seconds(5))
+            .build();
+
+        new Runner(options).run();
+    }
+
+    @Setup
+    public void setup() throws Throwable {
+        final String methodName = "name";
+        student = new Student("Alexander", "Biryukov");
+
+        method = Student.class.getDeclaredMethod(methodName);
+
+        MethodHandles.Lookup lookup = MethodHandles.lookup();
+        MethodType methodType = MethodType.methodType(String.class);
+        methodHandle = lookup.findVirtual(Student.class, methodName, methodType);
+
+        lambda = (StudentInterface) LambdaMetafactory.metafactory(
+            lookup,
+            methodName,
+            MethodType.methodType(StudentInterface.class),
+            MethodType.methodType(String.class, Student.class),
+            methodHandle,
+            MethodType.methodType(String.class, Student.class)
+        ).getTarget().invokeExact();
+    }
+
+    @Benchmark
+    public void directAccess(Blackhole bh) {
+        String name = student.name();
+        bh.consume(name);
+    }
+
+    @Benchmark
+    public void reflection(Blackhole bh) throws InvocationTargetException, IllegalAccessException {
+        String name = (String) method.invoke(student);
+        bh.consume(name);
+    }
+
+    @Benchmark
+    public void methodHandlers(Blackhole bh) throws Throwable {
+        String name = (String) methodHandle.invoke(student);
+        bh.consume(name);
+    }
+
+    @Benchmark
+    public void lambdaMetafactory(Blackhole bh) {
+        String name = lambda.name(student);
+        bh.consume(name);
+    }
+
+    public interface StudentInterface {
+        String name(Student student);
+    }
+
+    public record Student(String name, String surname) {
+    }
+}
